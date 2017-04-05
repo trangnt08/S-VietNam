55a663bbcd6c61456283092659a2c4a897dd5018
Nguyen Thanh Tung <jarvis@Nguyens-MacBook-Air.local>
2017-04-05 21:48:04 +0700

    add signin/signup login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  root 'sessions#new'
+
+  get '/home', to: 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
