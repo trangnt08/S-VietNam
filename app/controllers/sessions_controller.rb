d20072e594aecbe915619bbeb35bbbdcea61c1e7
Nguyen Thanh Tung <jarvis@Nguyens-MacBook-Air.local>
2017-04-06 04:37:16 +0700

    add remember me

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       flash[:success] = "Welcome to the S-Vietnam!"
       redirect_to user
     else
@@ -22,7 +23,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
+ 
