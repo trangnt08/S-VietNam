2b1ce81004436e535b10cc83096658aa0e8f3b10
Nguyen Thuy Trang <thuytrangnguyen1306@gmail.com>
2017-04-24 03:29:00 +0000

    upload image2

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,10 +1,2 @@
 module ImagesHelper
-    # def image_preview(f)
-	   # url, style =  f.object.image.present? ?  [f.object.image.url(:thumb), ''] : ['#','display:none;']
-	   # content_tag(:div, nil) do
-		  #  concat image_tag url , {id: 'img_prev', style: style}
-		  #  concat ''.html_safe
-		  #  concat  f.button 'Remove', {type: 'button', class: 'remove_image', style:'display:none;'}
-	   # end
-    # end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -2,6 +2,4 @@ class Image < ActiveRecord::Base
     mount_uploader :scenery_picture, ImageUploader
     
     # belongs_to :user
-    
-    # has_attached_file :scenery_picture
 end
