282d6bcce4283dd5b9516d2fe3d5c8f18620153c
Linh Tran <linhttd.bk@gmail.com>
2017-04-24 20:18:57 +0000

    search

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,15 @@
 class StaticPagesController < ApplicationController
   def home
+    @titles = "LATEST SCENERIES"
+    @sceneries = Image.all.order("created_at DESC")
+  if params[:search]
+     @sceneries = Image.search(params[:search]).order("created_at DESC")
+    if (@sceneries.count > 0)
+      @titles = "RESULT"
+    else
+      @titles = "NOT FOUND!!"
+    end
+  end
   end
 
   def help
--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,9 @@ class Image < ActiveRecord::Base
     mount_uploader :scenery_picture, ImageUploader
     
     # belongs_to :user
+    def self.search(search)
+      if search
+        where("scenery_name LIKE :search OR scenery_address LIKE :search", search:"%#{search}%")
+      end
+    end
 end
